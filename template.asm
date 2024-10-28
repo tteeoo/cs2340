5083253d9002096e398b10c6fe901532b91c6f30
Theo Henson <theodorehenson@protonmail.com>
2024-10-28 13:12:44 -0500

    Initial commit

---
+++ template.asm
@@ -0,0 +1,13 @@
+# CS 2340 Term Project
+# Author: Theo & Anthony
+# Date: Wed Nov 20 2024
+# Description: Multiplication matching game
+
+.include	"SysCalls.asm"
+.data
+	# Data segment
+
+.text
+
+	li $v0, SysExit	# syscall: exit 
+	syscall
