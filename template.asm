61038acafd2d8233bf27cef890e22fd8ad224984
Anthony Simmons <lckyclovrstudio@gmail.com>
2024-11-05 17:11:10 -0600

    update matrix

--- template.asm
+++ template.asm
@@ -6,8 +6,12 @@
 .include	"SysCalls.asm"
 .data
 	# Data segment
+	displayMatrix: .asciiz "04x3001204x4002005x405x50012002001x204x5002503x400080002001602x4"
+	binaryMatrix: .word 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	valueMatrix: .word 12,12,16,20,20,25,12,20,2,20,25,12,8,2,16,8
 
 .text
-
+	
+	
 	li $v0, SysExit	# syscall: exit 
 	syscall
