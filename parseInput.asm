85e65c86b49b3ead221dba8d5fd824f20d3cc8aa
Anthony Simmons <lckyclovrstudio@gmail.com>
2024-11-21 12:54:45 -0600

    Modules

---
+++ parseInput.asm
@@ -0,0 +1,46 @@
+parseInput:
+	li $v0, SysPrintString # Prompt for Cell #1 Number
+	la $a0, rowPrompt
+	syscall
+
+	li $v0, SysReadInt # Read in Cell #1 Number
+	syscall
+
+	blt $v0, 1, parseInput # Input validation
+	bgt $v0, 16, parseInput
+
+	move $s0,$v0 # Save Cell #1 value to $s0
+
+	li $v0, SysPrintString # Prompt for Cell #2 Number
+	la $a0, colPrompt
+	syscall
+	
+	li $v0, SysReadInt # Read in Cell #2 value
+	syscall
+	
+	blt $v0, 1, parseInput # Input validation
+	bgt $v0, 16, parseInput
+	
+	move $s1,$v0 # Save Cell #2 value to $s1
+	
+	li $v0, SysPrintString # Showing selected values
+	la $a0, selectedText
+	syscall
+			
+	li $v0, SysPrintInt
+	move $a0, $s0
+	syscall
+			
+	li $v0, SysPrintString # Showing selected values
+	la $a0, comma
+	syscall
+			
+	li $v0, SysPrintInt
+	move $a0, $s1
+	syscall
+			
+	li $v0, SysPrintString # Printing new line
+	la $a0, newLine 
+	syscall
+	
+jr $ra
---
+++ renderBoard.asm
@@ -0,0 +1,33 @@
+renderBoard:
+	li $t0, 0 # Counter for the loop
+	li $t1, 16 # Limit of the loop
+	cellLoop:
+		la $t2, displayMatrix # Grap the start of the display matrix
+		sll $t3, $t0, 2 # Counter * 4 = offset of cell
+		add $t2, $t2, $t3 # Label + offset = cell in memory
+		
+		li $t5, 0 # Counter for the loop
+		li $t6, 4 # Limit of the loop
+		charLoop:
+			add $t7, $t2, $t5 # Byte = cell + counter
+			lb $a0, 0($t7) # Load char to print
+			li $v0, SysPrintChar # Syscall for printing a character
+			syscall
+			addi $t5, $t5, 1 # Incremement counter
+		blt $t5, $t6, charLoop
+			
+		addi $t0, $t0, 1 # Incremement counter
+		 
+		# Printing a newline after every fourth cell
+		li $t2, 4
+		div $t0, $t2 # Divide counter by 4
+		mfhi $t3 # Move the remainder to $t3
+		bnez $t3, skipNewline # Skip newline printing if divisble by 4
+		
+		li $v0, SysPrintString # Syscall for printing a string
+		la $a0, newLine # Load address of the message
+		syscall
+		
+		skipNewline:
+	blt $t0, $t1, cellLoop
+jr $ra
